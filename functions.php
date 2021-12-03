f594c1fc68f4f328c2c3c1e640d6f780f8a0e2b5
Peter Smith <psmith@inviqa.com>
2021-12-03 09:09:55 +0000

    add some return types

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-function pipe($value)
+function pipe($value): callable
 {
     return match (is_callable($value)) {
         true => fn(callable $fn = null) => match ($fn) {
@@ -10,13 +10,12 @@ function pipe($value)
     };
 }
 
-$fold = fn(callable $f, mixed $carry = null) => function (iterable $collection) use ($f, $carry) {
+$fold = fn(callable $f, mixed $carry = null): callable => function (iterable $collection) use ($f, $carry) {
     foreach ($collection as $item) {
         $carry = $f($carry, $item);
     };
     return $carry;
 };
-
 
 $head = function (iterable $collection) {
     foreach ($collection as $item) {
@@ -34,29 +33,27 @@ $tail = function (iterable $collection): iterable {
     }
 };
 
-$reduce = fn(iterable $collection, callable $f) => $fold($f, $head($collection))($tail($collection));
+$reduce = fn(callable $f) => fn(iterable $collection) => $fold($f, $head($collection))($tail($collection));
 
-$sum = fn(iterable $collection) => $reduce(
-    $collection,
-    fn($carry, $item) => $item + $carry
-);
+$sum = fn(iterable $collection) => $reduce(fn($carry, $item) => $item + $carry)($collection);
 
-$count = fn(iterable $collection) => $fold(fn($carry, $item): int => $carry + 1, 0)($collection);
+$multiply = fn(iterable $collection) => $reduce(fn($carry, $item) => $item * $carry)($collection);
 
-$map = fn(callable $f) => function (iterable $collection) use ($f): iterable {
+$count = fn(iterable $collection): int => $fold(fn($carry, $item): int => $carry + 1, 0)($collection);
+
+$map = fn(callable $f): callable => function (iterable $collection) use ($f): iterable {
     foreach ($collection as $item) {
         yield $f($item);
     }
 };
 
-
-$filter = function (callable $predicate = null) {
+$filter = function (callable $predicate = null): callable {
     $predicate = match ($predicate) {
         null => fn($i) => $i !== null,
         default => $predicate,
     };
 
-    return function (iterable $collection) use ($predicate) {
+    return function (iterable $collection) use ($predicate): iterable {
         foreach ($collection as $item) {
             match ($predicate($item)) {
                 true => yield $item,
@@ -66,7 +63,7 @@ $filter = function (callable $predicate = null) {
     };
 };
 
-$window = fn(int $size, iterable $collection = null) => function (iterable $collection) use ($size) {
+$window = fn(int $size): callable => function (iterable $collection) use ($size): iterable {
     $buf = array_fill(0, $size, 0);
 
     foreach ($collection as $i => $item) {
